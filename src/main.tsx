8c4ed65d9cb3ac1a192924ef78a7e7974e196d98
Sergio Barreras <sergiobg.isc@gmail.com>
2025-07-02 10:43:52 -0700

    clase: Estructura inicial, estilos y fuente

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { GifsApp } from "./GifsApp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Hola mundo</h1>
+    <GifsApp />
   </React.StrictMode>
 );
