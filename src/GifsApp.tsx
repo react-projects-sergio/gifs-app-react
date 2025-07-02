726aac3eaafc5e75e78d8255112e2fdfdae4d502
Sergio Barreras <sergiobg.isc@gmail.com>
2025-07-02 11:06:23 -0700

    clase: Pensemos en componentes

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,46 +1,22 @@
+import { GifList } from "./gifs/components/GifList";
+import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { mockGifs } from "./mock-data/gifs.mock";
+import { CustomHeader } from "./shared/components/CustomHeader";
+import { SearchBar } from "./shared/components/SearchBar";
 
 export const GifsApp = () => {
   return (
     <>
-      {/* Header */}
-      <div className="content-center">
-        <h1>Buscador de Gifs</h1>
-        <p>Descubre y comparte el gif perfecto</p>
-      </div>
+      <CustomHeader
+        title="Buscador de Gifs"
+        description="Descubre y comparte el Gif perfecto"
+      />
 
-      {/* Search */}
+      <SearchBar placeholder="Buscar gifs" />
 
-      <div className="search-container">
-        <input type="text" placeholder="Buscar gifs" />
-        <button>Buscar</button>
-      </div>
+      <PreviousSearches />
 
-      {/* Búsquedas  previas */}
-
-      <div className="previous-searches">
-        <h2>Búsquedas previas</h2>
-        <ul className="previous-searches-list">
-          <li>Star Wars</li>
-          <li>Cyberpunk</li>
-          <li>Batman</li>
-          <li>Demon Slayer</li>
-        </ul>
-      </div>
-
-      {/* Gifs */}
-
-      <div className="gifs-container">
-        {mockGifs.map((gif) => (
-          <div key={gif.id} className="gif-card">
-            <img src={gif.url} alt={gif.title} />
-            <h3>{gif.title}</h3>
-            <p>
-              {gif.width}x{gif.height} (1.5mb)
-            </p>
-          </div>
-        ))}
-      </div>
+      <GifList gifs={mockGifs} />
     </>
   );
 };
---
+++ src/gifs/components/PreviousSearches.tsx
@@ -0,0 +1,13 @@
+export const PreviousSearches = () => {
+  return (
+    <div className="previous-searches">
+      <h2>Búsquedas previas</h2>
+      <ul className="previous-searches-list">
+        <li>Star Wars</li>
+        <li>Cyberpunk</li>
+        <li>Batman</li>
+        <li>Demon Slayer</li>
+      </ul>
+    </div>
+  );
+};
